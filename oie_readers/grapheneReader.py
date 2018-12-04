3d35b68500882ce7185d3e678a8431d07de95f8f
Mischn <zo.matthias@gmail.com>
2018-12-04 16:27:41 +0100

    Integrated Graphene

---
+++ oie_readers/grapheneReader.py
@@ -0,0 +1,59 @@
+import re, math
+from oie_readers.oieReader import OieReader
+from oie_readers.extraction import Extraction
+
+class GrapheneReader(OieReader):
+    
+    def __init__(self):
+        self.name = 'Graphene'
+    
+    def calc_confidence(self, text):
+        return 1./(0.005*len(text)+1)
+
+    def read(self, fn):
+        d = {}
+
+        with open(fn) as fin:
+            for line in fin:
+                line = line.strip('\n')
+
+                data = line.strip().split('\t')
+                if len(data) < 7:
+                    continue
+
+                arg2 = ''
+                context_args = []
+                if len(data) == 7:
+                    text, identifier, extraction_type, conf, context_layer, arg1, pred = data[:7]
+                else:
+                    text, identifier, extraction_type, conf, context_layer, arg1, pred, arg2 = data[:8]
+                    context_args = data[8:]
+                context_layer = int(context_layer)
+                confidence = self.calc_confidence(text)
+
+                simple_contexts = []
+                linked_contexts = []
+                separator = '\|\|'
+                for c in context_args:
+                    sm = re.match('^\W*S:(?P<class>\w+)\((?P<text>.*?)\)\W*$', c)
+                    lm = re.match('^\W*L:(?P<class>\w+)\((?P<arg1>.*?)' + separator + '(?P<pred>.*?)' + separator + '(?P<arg2>.*?)\)\W*$', c)
+                    if bool(sm):
+                        simple_contexts.append((sm.group('class'), sm.group('text')))
+                    if bool(lm):
+                        linked_contexts.append((lm.group('class'), lm.group('arg1'), lm.group('pred'), lm.group('arg2')))
+
+                # create extraction
+                curExtraction = Extraction(pred = pred, sent = text, confidence = float(confidence))
+                curExtraction.addArg(arg1)
+                curExtraction.addArg(arg2)
+
+                for cl, t in simple_contexts:
+                    curExtraction.addArg(t)
+
+                if False:
+                    for cl, a1, p, a2 in linked_contexts:
+                        curExtraction.addArg(' '.join([a1, p, a2]))
+
+                d[text] = d.get(text, []) + [curExtraction]
+
+        self.oie = d
